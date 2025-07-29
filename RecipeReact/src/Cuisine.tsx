c8a24000c056d86d32ba57e98098d865c52cdced
shiffygreen <shiffygreen73@gmail.com>
2025-07-28 20:12:27 -0400

    fixed picture sizing

--- RecipeReact/src/Cuisine.tsx
+++ RecipeReact/src/Cuisine.tsx
@@ -10,8 +10,13 @@ export default function CuisineButton({ cuisine, isSelected, onSelected }: Props
 
   return (
     <div onClick={() => onSelected(cuisine)} className={`btn ${isSelected ? 'btn-secondary' : ""} `}>
-      <figure className="figure">
-        <img src={`/images/cuisine-images/${cuisine.cuisineType.toLowerCase()}.jpg`} className="figure-img img-fluid rounded" alt="" />
+      <figure className="figure" style={{ width: "120px", height: "120px", margin: "0 auto" }}>
+        <img
+          src={`/images/cuisine-images/${cuisine.cuisineType.toLowerCase()}.jpg`}
+          className="figure-img img-fluid rounded"
+          alt=""
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
         <figcaption className="figure-caption text-center">{cuisine.cuisineType}</figcaption>
       </figure>
     </div>
--- RecipeReact/src/RecipeCard.tsx
+++ RecipeReact/src/RecipeCard.tsx
@@ -7,8 +7,13 @@ interface Props {
 
 export default function RecipeCard({ recipe }: Props): JSX.Element {
     return (
-        <div className="card" >
-            <img src={`./images/recipe-images/${recipe.recipeName.toLocaleLowerCase()}.jpg`} className="card-img-top" alt="..." />
+        <div className="card">
+            <img
+                src={`./images/recipe-images/${recipe.recipeName.toLocaleLowerCase()}.jpg`}
+                className="card-img-top"
+                alt="..."
+                style={{ width: "100%", height: "200px", objectFit: "cover" }}
+            />
             <div className="card-body">
                 <h5 className="card-title">{recipe.recipeName}</h5>
                 <p className="card-text"></p>
